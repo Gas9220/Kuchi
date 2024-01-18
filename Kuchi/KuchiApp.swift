97e5e5b49a4ea4ede16a018d7a6921c721cb6056
Gaspare <Gas9220@users.noreply.github.com>
2024-01-18 09:55:58 +0100

    Initial Commit

---
+++ Kuchi/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Kuchi
+//
+//  Created by Gaspare Monte on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
